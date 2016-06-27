eff313dd3aae3a9aeb6a156bd11634e4910e219d
jdokoop <orjuela.koop@gmail.com>
2016-06-27 13:54:55 -0600

    Added DCA analysis wrt BC and reco vertex

--- MonteCarloResolution.C
+++ MonteCarloResolution.C
@@ -11,16 +11,16 @@ TH1F *h_resol;
 TH1F *h_beam;
 
 //Vertexing resolution
-const float SIGMA_RES_X = 201.6;
-const float SIGMA_RES_Y = 156.9;
+const float SIGMA_RES_X = 1.2 * 129.5;
+const float SIGMA_RES_Y = 1.2 * 107.4;
 
 //Beam spot spread
-const float SIGMA_BEAM_X = 131.8;
-const float SIGMA_BEAM_Y = 114.6;
+const float SIGMA_BEAM_X = 129.5;
+const float SIGMA_BEAM_Y = 107.4;
 
-//Beam spot spread
-const float BEAM_CTR_X = 1612.0;
-const float BEAM_CTR_Y = 723.0;
+//Beam spot center
+const float BEAM_CTR_X = 1612.1;
+const float BEAM_CTR_Y = 722.5;
 
 //Number of events to simulate
 const int NPOINTS = 1e6;
@@ -38,6 +38,12 @@ TH1F *hResTrueReco;
 
 //Distribution of distances between the true vertex and the beam center
 TH1F *hResTrueBeamCenter;
+
+//DCA distribution wrt the reconstructed vertex
+TH1F *hDCAReco;
+
+//DCA distribution wrt the beam center
+TH1F *hDCABC;
 
 //-------------------------------
 // Functions
@@ -88,51 +94,96 @@ void plot()
 	hRecoVertexY->Draw();
 	hTrueVertexY->Draw("same");
 
+	//Plot summary as a function of the number of tracks in each arm
+	/*
+	float x[4] = {1, 2, 3, 4};
+	float y_reco_true[4] = {0.62, 0.72, 0.82, 0.93};
+	float y_bc_true[4] = {0.77, 0.73, 0.79, 0.79};
+
+	TGraph *g_reco_true = new TGraph(4, x, y_reco_true);
+	TGraph *g_bc_true = new TGraph(4, x, y_bc_true);
+
+	TCanvas *cSummary = new TCanvas("cSummary","cSummary",600,600);
+
+	g_reco_true->SetTitle("");
+	g_reco_true->SetMarkerStyle(20);
+	g_reco_true->SetMarkerColor(kBlue);
+	g_reco_true->SetLineColor(kBlue);
+	g_reco_true->GetXaxis()->SetTitle("Number of Vertex Tracks in Each Arm");
+	g_reco_true->GetXaxis()->SetTitleFont(62);
+	g_reco_true->GetXaxis()->SetLabelFont(62);
+	g_reco_true->GetYaxis()->SetTitle("Area Below 200");
+	g_reco_true->GetYaxis()->SetTitleOffset(1.85);
+	//g_reco_true->GetYaxis()->SetRangeUser(0, 0.025);
+	g_reco_true->GetYaxis()->SetTitleFont(62);
+	g_reco_true->GetYaxis()->SetLabelFont(62);
+	g_reco_true->Draw("AP");
+	g_bc_true->SetTitle("");
+	g_bc_true->SetMarkerStyle(20);
+	g_bc_true->SetMarkerColor(kRed);
+	g_bc_true->SetLineColor(kRed);
+	g_bc_true->GetXaxis()->SetTitleFont(62);
+	g_bc_true->GetXaxis()->SetLabelFont(62);
+	//g_bc_true->GetYaxis()->SetRangeUser(0, 0.025);
+	g_bc_true->GetYaxis()->SetTitleFont(62);
+	g_bc_true->GetYaxis()->SetLabelFont(62);
+	g_bc_true->Draw("P,same");
+	*/
+
 	TCanvas *cResiduals = new TCanvas("cResiduals", "cResiduals", 600, 400);
-	hResTrueReco->Scale(1.0/hResTrueReco->Integral());
-	hResTrueBeamCenter->Scale(1.0/hResTrueBeamCenter->Integral());
+	hDCAReco->Scale(1.0 / hDCAReco->Integral());
+	hDCABC->Scale(1.0 / hDCABC->Integral());
 	cResiduals->SetLeftMargin(0.2);
-	hResTrueReco->SetTitle("");
-	hResTrueReco->GetXaxis()->SetTitle("Distance [#mum]");
-	hResTrueReco->GetXaxis()->SetTitleFont(62);
-	hResTrueReco->GetXaxis()->SetLabelFont(62);
-	hResTrueReco->GetYaxis()->SetTitle("Probability");
-	hResTrueReco->GetYaxis()->SetTitleOffset(1.8);
-	hResTrueReco->GetYaxis()->SetTitleFont(62);
-	hResTrueReco->GetYaxis()->SetLabelFont(62);
-	hResTrueReco->GetYaxis()->SetRangeUser(0, hResTrueBeamCenter->GetMaximum() + 0.1*hResTrueBeamCenter->GetMaximum());
-	hResTrueReco->SetLineColor(kBlue);
-	hResTrueReco->Draw();
-	hResTrueBeamCenter->SetLineColor(kRed);
-	hResTrueBeamCenter->Draw("same");
+	hDCAReco->SetTitle("");
+	hDCAReco->GetXaxis()->SetTitle("|DCA| [#mum]");
+	hDCAReco->GetXaxis()->SetTitleFont(62);
+	hDCAReco->GetXaxis()->SetLabelFont(62);
+	hDCAReco->GetYaxis()->SetTitle("Probability");
+	hDCAReco->GetYaxis()->SetTitleOffset(1.8);
+	hDCAReco->GetYaxis()->SetTitleFont(62);
+	hDCAReco->GetYaxis()->SetLabelFont(62);
+
+	float max = (hDCAReco->GetMaximum() > hDCABC->GetMaximum()) ? hDCAReco->GetMaximum() : hDCABC->GetMaximum();
+	hDCAReco->GetYaxis()->SetRangeUser(0, max + 0.1 * max);
+	hDCAReco->SetLineColor(kBlue);
+	hDCAReco->Draw();
+	hDCABC->SetLineColor(kRed);
+	hDCABC->Draw("same");
 
 	TLegend *tlResiduals = new TLegend(0.6, 0.7, 0.85, 0.8);
-	tlResiduals->AddEntry(hResTrueReco, "RECO - TRUE", "L");
-	tlResiduals->AddEntry(hResTrueBeamCenter, "BC - TRUE", "L");
+	tlResiduals->AddEntry(hDCAReco, "RECO", "L");
+	tlResiduals->AddEntry(hDCABC, "BC", "L");
 	tlResiduals->SetLineColor(kWhite);
 	tlResiduals->SetTextSize(0.05);
 	tlResiduals->Draw("same");
-
-	float areaBelow200BC = hResTrueBeamCenter->Integral(1, hResTrueBeamCenter->FindBin(200.0)) / hResTrueBeamCenter->Integral();
-	float areaBelow200Reco = hResTrueReco->Integral(1, hResTrueReco->FindBin(200.0)) / hResTrueReco->Integral();
-
-	TLatex *tlatAreaBC = new TLatex(0.6, 0.4, Form("Area Below 200 = %.3g", areaBelow200BC));
-	tlatAreaBC->SetTextColor(kRed);
-	tlatAreaBC->SetTextSize(0.035);
-	tlatAreaBC->SetNDC(kTRUE);
-	tlatAreaBC->Draw("same");
-
-	TLatex *tlatAreaReco = new TLatex(0.6, 0.35, Form("Area Below 200 = %.3g", areaBelow200Reco));
-	tlatAreaReco->SetTextColor(kBlue);
-	tlatAreaReco->SetTextSize(0.035);
-	tlatAreaReco->SetNDC(kTRUE);
-	tlatAreaReco->Draw("same");
-
-	TLine *tline200 = new TLine(200, 0, 200, hResTrueBeamCenter->GetMaximum() + 0.1*hResTrueBeamCenter->GetMaximum());
-	tline200->SetLineStyle(2);
-	tline200->Draw("same");
 }
 
+/*
+ * Get the distance of closest approach between the line defined by angle theta and point (ax, ay) and point (px, py)
+ */
+float getDCA(float theta, float px, float py, float ax, float ay)
+{
+	//Line parameter defining the point of closest approach to (x,y)
+	float t = ((px * TMath::Cos(theta) + py * TMath::Sin(theta)) - (ax * TMath::Cos(theta) + ay * TMath::Sin(theta))) / (TMath::Cos(theta) * TMath::Cos(theta) + TMath::Sin(theta) * TMath::Sin(theta));
+
+	//Point of closest approach
+	float dcax = ax + t * TMath::Cos(theta);
+	float dcay = ay + t * TMath::Sin(theta);
+
+	//Check that the lines are perpendicular
+	float testNorm = TMath::Sqrt((px - dcax) * (px - dcax) + (py - dcay) * (py - dcay));
+	float ang = (1.0 / testNorm) * ((px - dcax) * TMath::Cos(theta) + (py - dcay) * TMath::Sin(theta));
+
+	//Distance between (dcax, dcay) and (px,py)
+	float dca = TMath::Sqrt((dcax - px) * (dcax - px) + (dcay - py) * (dcay - py));
+
+	return dca;
+}
+
+/*
+ * Run N simulated events by sampling a true and a reconstructed vertex, with a straight track from the true vertex
+ * Compute the distance of closest approach between the track and the beam center, and the track and the reconstructed vertex
+ */
 void MonteCarloResolution()
 {
 	//Initialize variables
@@ -142,6 +193,8 @@ void MonteCarloResolution()
 	hRecoVertexY       = new TH1F("hRecoVertexY", "hRecoVertexY", 150, BEAM_CTR_Y - 5 * SIGMA_BEAM_Y, BEAM_CTR_Y + 5 * SIGMA_BEAM_Y);
 	hTrueVertexX       = new TH1F("hTrueVertexX", "hTrueVertexX", 150, BEAM_CTR_X - 5 * SIGMA_BEAM_X, BEAM_CTR_X + 5 * SIGMA_BEAM_X);
 	hTrueVertexY       = new TH1F("hTrueVertexY", "hTrueVertexY", 150, BEAM_CTR_Y - 5 * SIGMA_BEAM_Y, BEAM_CTR_Y + 5 * SIGMA_BEAM_Y);
+	hDCAReco           = new TH1F("hDCAReco", "hDCAReco", 500, 0, 600);
+	hDCABC             = new TH1F("hDCABC", "hDCABC", 500, 0, 600);
 
 	TRandom rndm(0);
 
@@ -155,6 +208,14 @@ void MonteCarloResolution()
 		//Generate the reconstructed vertex by sampling around the collision point
 		float vtx_reco_x = rndm.Gaus(vtx_true_x, SIGMA_RES_X);
 		float vtx_reco_y = rndm.Gaus(vtx_true_y, SIGMA_RES_Y);
+
+		//Generate straight track originating from the true vertex, parametrized by polar angle
+		float trackAngle = rndm.Uniform(2 * TMath::Pi());
+		float dcaRecoVtx = getDCA(trackAngle, vtx_reco_x, vtx_reco_y, vtx_true_x, vtx_true_y);
+		float dcaBC      = getDCA(trackAngle, BEAM_CTR_X, BEAM_CTR_Y, vtx_true_x, vtx_true_y);
+
+		hDCAReco->Fill(dcaRecoVtx);
+		hDCABC->Fill(dcaBC);
 
 		hRecoVertexX->Fill(vtx_reco_x);
 		hRecoVertexY->Fill(vtx_reco_y);
